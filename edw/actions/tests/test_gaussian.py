a0675a0a821dc33a2d6300b9064f2f5c10db2369
Logan Ward <ward.logan.t@gmail.com>
2019-10-21 14:54:40 -0500

    Made the unit test more meaningful
    Was otherwise parsing an empty molecule

--- edw/actions/tests/test_gaussian.py
+++ edw/actions/tests/test_gaussian.py
@@ -13,6 +13,7 @@ def test_parse():
 
 def test_multistep_relaxation():
     mol = geometry.smiles_to_conformers('C', 1)[0]
+    mol = geometry.mol_to_xyz(mol)
 
     input_file = gaussian.make_robust_relaxation_input(mol)
     assert len([x for x in input_file.split('\n') if 'link1' in x]) == 3
