2e8ad238e863b10e06d1d88006f28792c28f550b
Logan Ward <ward.logan.t@gmail.com>
2019-11-06 16:40:51 -0600

    Overhauled the script that corrects geometries

--- scripts/process_charged_geometries.py
+++ scripts/process_charged_geometries.py
@@ -6,85 +6,17 @@ from parsl.launchers import SrunLauncher
 from parsl.addresses import address_by_hostname
 from parsl.config import Config
 from concurrent.futures import as_completed
-from edw.actions import cclib, mongo, pybel
+from edw.actions import mongo, gaussian
 from edw.parsl import apps
 from pymongo import MongoClient
 from gridfs import GridFS
-from math import isclose
 from tqdm import tqdm
-import numpy as np
 import parsl
 import json
 
-# Connect to Mongo and GridFS
-client = MongoClient('localhost')
-collection = mongo.initialize_collection(client)
-gridfs = GridFS(client.get_database('jcesr'))
-
-# Query for completed relaxation
-query = {
-    'calculation.oxidized_b3lyp': {'$exists': True},
-    'calculation.reduced_b3lyp': {'$exists': True}
-}
-n_records = collection.count_documents(query)
-cursor = collection.find(query)
-print(f'Found {n_records} records with complete b3lyp calculations for both states')
-
-# Parse the output
-failures = {}
-for record in tqdm(cursor, total=n_records):
-    for state in ['reduced', 'oxidized']:
-        # Get the smiles
-        smiles = record['identifiers']['smiles']
-        inchi_key = record['inchi_key']
-        tag = ":".join((inchi_key, state))
-
-        # Get the output file
-        output_file = record['calculation'][f'{state}_b3lyp']['output_file']
-        output_file = gridfs.get(output_file)
-        output_file = output_file.read().decode()
-
-        # Parse the output file
-        try:
-            cjson = cclib.get_chemical_json(output_file)
-        except BaseException:
-            failures[tag] = 'Output failed to parse. Failure'
-            continue
-
-        # Get the geometry
-        neutral_geom = record['geometry']['neutral']
-        relaxed_geom = cclib.get_relaxed_structure(cjson)
-        mongo.add_geometry(collection, inchi_key, state, relaxed_geom)
-        if isclose(pybel.get_rmsd(neutral_geom, relaxed_geom), 0):
-            failures[tag] = 'Geometry did not change'
-            continue
-
-
-        # Check whether it converged
-        if not cjson['optimization']['done']:
-            failures[tag] = 'Optimization is incomplete'
-            continue
-
-        # Check whether the frequencies are all positive
-        freqs = cjson['vibrations']['frequencies']
-        positive_freqs = all(map(lambda x: x > 0, freqs))
-        if not positive_freqs:
-            failures[tag] = f'Negative frequencies. Minimum: {np.min(freqs)}'
-            continue
-
-
-print(f'{len(failures)} failures detected')
-
-# Resubmit the failures
-if len(failures) == 0:
-    exit()
-
-# Write the failures to disk
-with open('failures.json', 'w') as fp:
-    json.dump(failures, fp, indent=2)
-
 # Define how to launch Gaussian
 gaussian_cmd = ['g16']
+
 
 # Make a executor
 config = Config(
@@ -130,27 +62,68 @@ for app_name in apps.__all__:
         app.executors = worker_execs
         print(f'Assigned app {app_name} to executors: {app.executors}')
 
-# Assemble the workflow
+
+# Connect to Mongo and GridFS
+client = MongoClient('localhost')
+collection = mongo.initialize_collection(client)
+gridfs = GridFS(client.get_database('jcesr'))
+
+# Query for completed relaxation
+query = {
+    'calculation.oxidized_b3lyp': {'$exists': True},
+    'calculation.reduced_b3lyp': {'$exists': True}
+}
+n_records = collection.count_documents(query)
+cursor = collection.find(query)
+print(f'Found {n_records} records with complete b3lyp calculations for both states')
+
+# Parse the output
+failures = []
 jobs = []
-for failure in tqdm(failures, desc='Submitted'):
-    # Get the associated record
-    inchi_key, state = failure.split(":")
-    record = collection.find_one({'inchi_key': inchi_key})
+for record in tqdm(cursor, total=n_records):
+    for state in ['reduced', 'oxidized']:
+        # Get the smiles
+        smiles = record['identifiers']['smiles']
+        inchi_key = record['inchi_key']
+        tag = ":".join((inchi_key, state))
 
-    # Determine the charge
-    if state == 'oxidized':
-        charge = 1
-    elif state == 'reduced':
-        charge = -1
-    else:
-        raise ValueError('State not recognized')
+        # Determine the charge
+        charge = 1 if state == 'oxidized' else -1
 
-    # Submit a calculation
-    charged_calc = apps.relax_gaussian(inchi_key, record['geometry'][state],
-                                       gaussian_cmd, charge=charge, functional='B3LYP')
-    data = apps.match_future_with_inputs((inchi_key, charge), charged_calc)
-    jobs.append(data)
+        # Get the output file
+        calc_name = f'{state}_b3lyp'
+        output_file = record['calculation'][calc_name]['output_file']
+        output_file = gridfs.get(output_file)
+        output_file = output_file.read().decode()
 
+        # Check if the relaxation completed successfully
+        converged, new_structure = gaussian.validate_relaxation(output_file)
+
+        # If converged, store the result. We're done!
+        if converged:
+            collection.update_one({'inchi_key': inchi_key},
+                                  {'$set':
+                                      {f'calculation.{calc_name}': {
+                                          'validated': True
+                                  }}})
+            mongo.add_geometry(collection, inchi_key, state, new_structure)
+        else:
+            failures.append(inchi_key)
+            charged_calc = apps.relax_gaussian(inchi_key, new_structure,
+                                               gaussian_cmd, charge=charge,
+                                               functional='B3LYP')
+            data = apps.match_future_with_inputs((inchi_key, charge), charged_calc)
+            jobs.append(data)
+
+# Summarize and exit if no work to do!
+print(f'{len(failures)} failures detected')
+
+if len(failures) == 0:
+    exit()
+
+# Write the failures to disk
+with open('failures.json', 'w') as fp:
+    json.dump(failures, fp, indent=2)
 
 # Wait until the complete
 for j in tqdm(as_completed(jobs), desc='Completed', total=len(jobs)):
@@ -164,4 +137,3 @@ for j in tqdm(as_completed(jobs), desc='Completed', total=len(jobs)):
     mongo.add_calculation(collection, gridfs, inchi_key, name,
                           result['input_file'], result['output_file'], 'Gaussian',
                           result['successful'])
-
