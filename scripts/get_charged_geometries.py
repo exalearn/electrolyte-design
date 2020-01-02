b8bc9ea79aaf72a976f1fe23b888a8e58cb7242a
Logan Ward <ward.logan.t@gmail.com>
2020-01-02 17:32:44 -0600

    Revised to use the QCArchive Collection

--- scripts/get_charged_geometries.py
+++ scripts/get_charged_geometries.py
@@ -1,10 +1,13 @@
-"""Run geometry relaxations for the first 1000 molecules in our database"""
+"""Add some molecules"""
 from qcportal.client import FractalClient
 from qcportal import Molecule
+import pandas as pd
 import argparse
 
 # Parse input arguments
 arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--limit', help='Number of molecules to add. -1 to add all', default=1, type=int)
+arg_parser.add_argument('qm9_path', help='Path to the QM9 data file')
 arg_parser.add_argument('password', help='Password for the service')
 arg_parser.add_argument('--address', help='Address to QCFractal service',
                         default='localhost:7874', type=str)
@@ -12,35 +15,59 @@ args = arg_parser.parse_args()
 
 # Make the FractalClient
 client = FractalClient(args.address, verify=False, username='user', password=args.password)
-mols = client.query_molecules(limit=1)
 
-# Make a charged version of each molecule and submit the molecule and charge
-for name, charge in zip(['reduced', 'oxidized'], [-1, 1]):
-    new_mols = []
-    for mol in mols:
-        new_mol = Molecule.from_data(
-            mol.to_string('xyz'),
-            molecular_charge=charge,
-            name=f'{mol.name}_{name}'
-        )
-        new_mols.append(new_mol)
+# Assemble the dataset
+coll = client.get_collection('OptimizationDataset', name='NWCHem+Geometric Relaxation')
+specs = coll.list_specifications(description=False)
+print(f'Found the following specifications: {specs}')
 
-    # Add the molecules to server
-    result = client.add_molecules(new_mols)
-    print(f'Molecules added: {result}')
+if "default" not in specs:
+    print('Setting up computation settings...')
 
-    # Submit optimization records
     spec = {
-        "keywords": None,
-        "qc_spec": {
-            "driver": "gradient",
-            "method": "b3lyp",
-            "basis": "sto-3g",
-            "program": "psi4"
-        },
+        'name': 'default',
+        'description': 'Geometric + NWChem/B3LYP/6-31g(2df,p).',
+        'optimization_spec': {
+            'program': 'geometric',
+            'keywords': None
+        }, 'qc_spec': {
+            'driver': 'gradient',
+            'method': 'b3lyp',
+            'basis': '6-31g(2df,p)',
+            'program': 'nwchem'
+        }
     }
-    result = client.add_compute(program='psi4', driver='energy', method='b3lyp',
-                                basis='6-31G(2df,p)', molecule=new_mols)
-    print(f'Started compute: {result}')
-    result = client.add_procedure("optimization", "geometric", spec, new_mols)
-    print(f'Started procedure: {result}')
+
+    coll.add_specification(**spec)
+    coll.save()
+
+# Get the current list of molecules
+existing_mols = coll.df.index
+
+# Load in QM9
+data = pd.read_json(args.qm9_path, lines=True)
+if args.limit > 0:
+    data = data.head(args.limit)
+
+# Add neutral charged versions of the molecules to the collection
+mols_to_add = [Molecule.from_data(xyz, name=f'{smiles}_neutral') for xyz, smiles in zip(data['xyz'], data['smiles_0'])]
+
+# Add the charged versions
+for name, charge in zip(['reduced', 'oxidized'], [-1, 1]):
+    for xyz, smiles in zip(data['xyz'], data['smiles_0']):
+        new_mol = Molecule.from_data(
+            xyz,
+            molecular_charge=charge,
+            name=f'{smiles}_{name}'
+        )
+        mols_to_add.append(new_mol)
+
+# Submit the molecules to server
+for mol in mols_to_add:
+    if mol.name not in existing_mols:
+        coll.add_entry(mol.name, mol, save=False)
+coll.save()
+
+# Trigger the calculations
+n_started = coll.compute('default')
+print(f'Started {n_started} calculations')
