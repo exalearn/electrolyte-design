0ff8519f5a1262b7949db7d8637e9f9a74a4d447
Logan Ward <ward.logan.t@gmail.com>
2020-01-17 13:05:28 -0600

    Added files to write out b3lyp calculations

--- scripts/write_out_b3lyp.py
+++ scripts/write_out_b3lyp.py
@@ -18,8 +18,8 @@ gridfs = GridFS(client.get_database('jcesr'))
 
 # Query for completed calculations
 query = {
-    'calculation.oxidized_g4mp2': {'$exists': True},
-    'calculation.reduced_g4mp2': {'$exists': True}
+    'calculation.oxidized_b3lyp': {'$exists': True},
+    'calculation.oxidized_b3lyp': {'$exists': True}
 }
 total_count = collection.count_documents(query)
 print(f'Found {total_count} completed calculations')
@@ -29,30 +29,23 @@ cursor = collection.find(query)
 did_not_complete = []
 for record in tqdm(cursor, total=total_count, desc='Extracting energies'):
     rid = record['inchi_key']
-    for calc_name in ['oxidized_g4mp2', 'reduced_g4mp2']:
+    for calc_name in ['oxidized_b3lyp', 'reduced_b3lyp']:
         # Determine the charge state
         charge_state = calc_name.split("_")[0]
         if calc_name not in record['calculation']:
-            raise ValueError(f'Missing {calc_name}')
+            did_not_complete.append(rid)
+            continue
 
         # Read in the output file
         output_text = gridfs.get(record['calculation'][calc_name]['output_file']).read().decode()
 
         # Get the B3LYP energy
-        b3lyp_match = re.search(r'E\(UB3LYP\) = +([-\d.]+)', output_text)
-        if b3lyp_match is None:
+        b3lyp_match = re.findall(r'Sum of electronic and zero-point Energies=           (?P<energy>-[\d\.]+)', output_text)
+        if len(b3lyp_match) == 0:
             did_not_complete.append(rid)
             continue
-        b3lyp_energy = float(re.sub(r'\s', '', b3lyp_match.group(1)))
+        b3lyp_energy = float(re.sub(r'\s', '', b3lyp_match[-1]))
         mongo.add_property(collection, rid, f'u0_{charge_state}', 'b3lyp', b3lyp_energy)
-
-        # Read the G4MP2 energy
-        g4mp2_match = re.search(r'G4MP2=([-\d.\s]+)', output_text)
-        if g4mp2_match is None:
-            did_not_complete.append(rid)
-            continue
-        g4mp2_energy = float(re.sub(r'\s', '', g4mp2_match.group(1)))
-        mongo.add_property(collection, rid, f'u0_{charge_state}', 'g4mp2', g4mp2_energy)
 
 print(f'Errors in {len(did_not_complete)} calculations')
 
@@ -77,5 +70,5 @@ for record in tqdm(cursor, total=total_count, desc='Saving results'):
 
     output.append(summary)
 
-pd.DataFrame(output).to_csv('g4mp2_results.csv', index=False)
+pd.DataFrame(output).to_csv('b3lyp_results.csv', index=False)
 
