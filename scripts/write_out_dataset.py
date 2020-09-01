ecc08baba306fdf363c9105f02007252165ac0ea
Logan Ward <ward.logan.t@gmail.com>
2020-09-01 18:31:42 -0500

    Option for deleting failed records

--- scripts/write_out_dataset.py
+++ scripts/write_out_dataset.py
@@ -9,6 +9,7 @@ import re
 # Parse input arguments
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--port', help='MongoDB port', default=None, type=int)
+arg_parser.add_argument('--restart', action='store_true', help='Delete failed calculations so they can be restarted')
 args = arg_parser.parse_args()
 
 # Make the MongoClient
@@ -41,7 +42,7 @@ for record in tqdm(cursor, total=total_count, desc='Extracting energies'):
         # Get the B3LYP energy
         b3lyp_match = re.search(r'E\(UB3LYP\) = +([-\d.]+)', output_text)
         if b3lyp_match is None:
-            did_not_complete.append(rid)
+            did_not_complete.append((rid, calc_name))
             continue
         b3lyp_energy = float(re.sub(r'\s', '', b3lyp_match.group(1)))
         mongo.add_property(collection, rid, f'u0_{charge_state}', 'b3lyp', b3lyp_energy)
@@ -49,7 +50,7 @@ for record in tqdm(cursor, total=total_count, desc='Extracting energies'):
         # Read the ZPE
         zpe_match = re.search(r'E\(ZPE\)= +([-\d.]+)', output_text)
         if zpe_match is None:
-            did_not_complete.append(rid)
+            did_not_complete.append((rid, calc_name))
             continue
         zpe = float(re.sub(r'\s', '', zpe_match.group(1)))
         mongo.add_property(collection, rid, f'zpe_{charge_state}', 'g4mp2', zpe)
@@ -58,12 +59,30 @@ for record in tqdm(cursor, total=total_count, desc='Extracting energies'):
         # Read the G4MP2 energy
         g4mp2_match = re.search(r'G4MP2=([-\d.\s]+)', output_text)
         if g4mp2_match is None:
-            did_not_complete.append(rid)
+            did_not_complete.append((rid, calc_name))
             continue
         g4mp2_energy = float(re.sub(r'\s', '', g4mp2_match.group(1)))
         mongo.add_property(collection, rid, f'u0_{charge_state}', 'g4mp2', g4mp2_energy)
 
+
 print(f'Errors in {len(did_not_complete)} calculations')
+
+# Restart them if need be
+if args.restart:
+    for inchi, calc_name in tqdm(did_not_complete, desc='Restarting'):
+        # Get the failed record
+        record = collection.find_one({'inchi_key': inchi})
+
+        # Delete the files
+        calc_info = record['calculation'][calc_name]
+        for f in ['input_file', 'output_file']:
+            f = calc_info[f]
+            gridfs.delete(f)
+
+        # Unset the record
+        collection.update_one(filter={'inchi_key': inchi}, 
+                update={'$unset': {f'calculation.{calc_name}': ''}})
+
 
 # Loop through and dump out the energies
 cursor = collection.find(query)
