51304a0bc46c6027e48841944b2fead53d19118e
Logan Ward <ward.logan.t@gmail.com>
2020-12-02 09:06:03 -0600

    Be able to add Hs into molecule

--- moldesign/sample/rl/envs/rewards/tests/test_mpnn.py
+++ moldesign/sample/rl/envs/rewards/tests/test_mpnn.py
@@ -2,8 +2,9 @@ from math import isclose
 import pickle as pkl
 
 from moldesign.sample.rl.envs.rewards.mpnn import MPNNReward
-
 from moldesign.utils.conversions import convert_smiles_to_nx
+
+from pytest import raises
 
 
 def test_mpnn_reward(model, atom_types, bond_types):
@@ -22,3 +23,11 @@ def test_pickle(model, atom_types, bond_types):
     reward2 = pkl.loads(pkl.dumps(reward))
 
     assert isclose(reward(graph), reward2(graph), abs_tol=1e-6)
+
+
+def test_add_hs(model, atom_types, bond_types):
+    reward = MPNNReward(model, atom_types, bond_types, add_hs=True)
+    graph = convert_smiles_to_nx('C')
+    with raises(ValueError, match=r"^1 is not in"):
+        reward(graph)
+
