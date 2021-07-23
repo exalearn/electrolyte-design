6b7e85d75e4dfb4feda41c8e7492106a6f13cdca
Logan Ward <ward.logan.t@gmail.com>
2021-07-23 15:36:46 +0000

    Large update to initial dataset
    - Add diffuse basis set
    - Add vertical IP and EA

--- notebooks/ip-collection/load-qcfractal-into-mongo.py
+++ notebooks/ip-collection/load-qcfractal-into-mongo.py
@@ -2,7 +2,7 @@
 from tqdm import tqdm
 
 from moldesign.simulate.thermo import compute_frequencies
-from moldesign.simulate.qcfractal import GeometryDataset, SolvationEnergyDataset, HessianDataset
+from moldesign.simulate.qcfractal import GeometryDataset, SolvationEnergyDataset, HessianDataset, SinglePointDataset
 from moldesign.store.mongo import MoleculePropertyDB
 
 # Log in to MongoDB
@@ -16,6 +16,9 @@ nbn_geom = GeometryDataset('Electrolyte Geometry NWChem, 6-31G(2df,p)', 'normal_
 sbn_hess = HessianDataset('Electrolyte Hessian', 'nwchem', 'small_basis')
 nbn_hess = HessianDataset('Electrolyte Hessian, 6-31G(2df,p)', 'nwchem', 'normal_basis')
 
+smb_xtb_vert = SinglePointDataset('Electrolyte XTB Neutral Geometry, Small-Basis Energy', 'nwchem', 'small_basis',)
+nbn_xtb_vert = SinglePointDataset('Electrolyte XTB Neutral Geometry, Normal-Basis Energy', 'nwchem', 'normal_basis')
+
 xtb_solv = SolvationEnergyDataset('EDW XTB Solvation Energy', 'xtb', 'xtb')
 sbn_solv = SolvationEnergyDataset('EDW NWChem Solvation Energy', 'nwchem', 'small_basis')
 
@@ -23,40 +26,36 @@ sbn_solv = SolvationEnergyDataset('EDW NWChem Solvation Energy', 'nwchem', 'smal
 for geom in [xtb_geom, sbn_geom, nbn_geom]:
     # Load the records
     records = geom.get_complete_records()
-    mols = geom.get_geometries()
 
     # Put them in the database
-    for name, record in tqdm(records.items(), desc=f'geom: {geom.qc_spec}'):
+    for name, record in tqdm(records.items(), desc=f'geom: {geom.qc_spec}', total=len(records)):
         inchi, state = name.split("_")
 
         # Store the geometry and total energy
         mol = mongo.get_molecule_record(inchi=inchi)
         mol.subsets.append('initial')
-        mol.geometries[state][geom.qc_spec] = mols[inchi][state].to_string('xyz')
-        mol.total_energies[state][geom.qc_spec] = record.get_final_energy()
+        mol.add_geometry(record, overwrite=True)
 
         # Update the MongoDB
         mongo.update_molecule(mol)
 
+
+# Do the vertical energies
+for vert in [smb_xtb_vert, nbn_xtb_vert]:
+    pass
+
 # Now, do the Hessians
 for hess in [nbn_hess, sbn_hess]:
-    # Pre-load the geometries
-    mols = hess.get_geometries()
+    records = hess.get_complete_records()
 
     # Add each hessian
-    for name, record in tqdm(hess.get_complete_records().items(), desc=f'hess: {hess.qc_spec}'):
+    for name, record in tqdm(records, desc=f'hess: {hess.qc_spec}', total=len(records)):
         inchi, state = name.split("_")
 
         # Get the current record for this object
         #  Only retrieve the data needed to compute the atomization energy and ZPE
         mol = mongo.get_molecule_record(inchi=inchi)
+        mol.add_single_point(record)
 
-        # Compute the vibrational frequencies, atomization and ZPE
-        freqs = compute_frequencies(record.return_result, mols[inchi][state])
-        if state not in mol.vibrational_modes:
-            mol.vibrational_modes[state] = {}
-        mol.vibrational_modes[state][hess.qc_spec] = freqs.tolist()
-        mol.update_thermochem(verbose=False)
-        assert hess.qc_spec in mol.zpes[state]
-
+        # Update the record, which will compute the ZPE and such
         mongo.update_molecule(mol)
--- notebooks/ip-collection/update-calculations.py
+++ notebooks/ip-collection/update-calculations.py
@@ -1,36 +1,84 @@
 """Submit new calculations"""
+from typing import List
 
-from moldesign.simulate.qcfractal import GeometryDataset, SolvationEnergyDataset, HessianDataset
+from qcelemental.models import Molecule
+
+from moldesign.simulate.qcfractal import GeometryDataset, SolvationEnergyDataset, HessianDataset, SinglePointDataset
 
 # List the solvents to use
-_xtb_solvs = ['water', 'dmso', 'acetone', 'acetonitrile']
-_nwc_solvs = ['water', 'dmse', 'acetone', 'ethanol', 'acetntrl']
+_xtb_solvs = ['water', 'acetonitrile']
+_nwc_solvs = ['acetntrl']
 
 # Get the geometry datasets
 xtb_geom = GeometryDataset('Electrolyte Geometry XTB', 'xtb')
-sbn_geom = GeometryDataset('Electrolyte Geometry NWChem', 'small_basis')
+smb_geom = GeometryDataset('Electrolyte Geometry NWChem', 'small_basis')
 nbn_geom = GeometryDataset('Electrolyte Geometry NWChem, 6-31G(2df,p)', 'normal_basis')
+
+smb_xtb_vert = SinglePointDataset('Electrolyte XTB Neutral Geometry, Small-Basis Energy', 'nwchem', 'small_basis',)
+nbn_xtb_vert = SinglePointDataset('Electrolyte XTB Neutral Geometry, Normal-Basis Energy', 'nwchem', 'normal_basis')
+dif_xtb_vert = SinglePointDataset('Electrolyte XTB Neutral Geometry, Diffuse-Basis Energy', 'nwchem', 'diffuse_basis')
+
+smb_smb_vert = SinglePointDataset('Electrolyte SMB Neutral Geometry, Small-Basis Energy', 'nwchem', 'small_basis')
+nbn_smb_vert = SinglePointDataset('Electrolyte SMB Neutral Geometry, Normal-Basis Energy', 'nwchem', 'normal_basis')
 
 sbn_hess = HessianDataset('Electrolyte Hessian', 'nwchem', 'small_basis')
 nbn_hess = HessianDataset('Electrolyte Hessian, 6-31G(2df,p)', 'nwchem', 'normal_basis')
 
-xtb_solv = SolvationEnergyDataset('EDW XTB Solvation Energy', 'xtb', 'xtb')
-sbn_solv = SolvationEnergyDataset('EDW NWChem Solvation Energy', 'nwchem', 'small_basis')
+xtb_solv = SolvationEnergyDataset('EDW XTB Solvation Energy', 'xtb', 'xtb', _xtb_solvs)
+smb_solv = SolvationEnergyDataset('EDW NWChem Solvation Energy', 'nwchem', 'small_basis', _nwc_solvs)
+nbn_solv = SolvationEnergyDataset('Electrolyte Normal-Basis Solvation Energy', 'nwchem', 'normal_basis', _nwc_solvs)
 
-# Start charged geometries if a previous level of theory has completed
-for geom in [xtb_geom, sbn_geom, nbn_geom]:
+smb_xtb_vert_solv = SolvationEnergyDataset('Electrolyte XTB Neutral Geometry, Small-Basis Solvation Energy', 'nwchem', 'small_basis', _nwc_solvs)
+nbn_xtb_vert_solv = SolvationEnergyDataset('Electrolyte XTB Neutral Geometry, Normal-Basis Solvation Energy', 'nwchem', 'normal_basis', _nwc_solvs)
+dif_xtb_vert_solv = SolvationEnergyDataset('Electrolyte XTB Neutral Geometry, Diffuse-Basis Solvation Energy', 'nwchem', 'diffuse_basis', _nwc_solvs, create=True)
+
+# Start charged geometries if a neutral is completed
+for geom in [xtb_geom, smb_geom, nbn_geom]:
     n_started = geom.start_charged_geometries()
     print(f'Started {n_started} charged geometries for {geom.coll.name}')
 
 # Begin Hessian calculations for the NWChem runs
-for hess, geom in [(sbn_hess, sbn_geom), (nbn_hess, nbn_geom)]:
+for hess, geom in [(sbn_hess, smb_geom), (nbn_hess, nbn_geom)]:
     was_added = hess.add_geometries(geom)
     print(f'Added {was_added} geometries from {geom.coll.name} to {hess.coll.name}')
     num_started = hess.start_computation()
     print(f'Started {num_started} computations for {hess.coll.name}')
 
+
+# Start computations with neutral geometries
+def submit_vertical_geometries(geom_dataset: GeometryDataset, vert_datasets: List[SinglePointDataset]):
+    all_geoms = geom_dataset.get_geometries()
+    print(f'Found {len(all_geoms)} molecules in {geom_dataset.coll.name}')
+    for inchi, geoms in all_geoms.items():
+        # Get the neutral geometry
+        if 'neutral' not in geoms:
+            continue
+        geom = geoms['neutral'].to_string('xyz')
+
+        # Start the neutral geometry in all three charge states
+        for postfix, charge in zip(['reduced', 'neutral', 'oxidized'], [-1, 0, 1]):
+            # Make a name
+            if charge != 0:
+                identifier = f'{inchi}_xtb_neutral_{postfix}'
+            else:
+                identifier = f'{inchi}_xtb_neutral'
+            new_geom = Molecule.from_data(geom, 'xyz', molecular_charge=charge, name=identifier)
+            # Loop over the different levels of accuracy
+            for vert in vert_datasets:
+                vert.add_molecule(new_geom, inchi, save=False)
+
+    for vert in vert_datasets:  # Start the computations
+        vert.coll.save()
+        vert_started = vert.start_computation()
+        print(f'Started {vert_started} computations for {vert.coll.name}')
+
+
+submit_vertical_geometries(xtb_geom, [smb_xtb_vert, nbn_xtb_vert, dif_xtb_vert])
+submit_vertical_geometries(smb_geom, [smb_smb_vert, nbn_smb_vert])
+
+
 # Pass geometries from one level forward to the next
-for start_geom, end_geom in [(xtb_geom, sbn_geom), (sbn_geom, nbn_geom)]:
+for start_geom, end_geom in [(xtb_geom, smb_geom), (smb_geom, nbn_geom)]:
     n_added = 0
     for inchi, geoms in start_geom.get_geometries().items():
         if 'neutral' in geoms:
@@ -41,17 +89,42 @@ for start_geom, end_geom in [(xtb_geom, sbn_geom), (sbn_geom, nbn_geom)]:
     print(f'Added {n_added} geometries from {start_geom.coll.name} to {end_geom.coll.name}')
     n_started = end_geom.start_compute()
     print(f'Started {n_started} neutral molecule relaxations for {end_geom.coll.name}')
-exit()
 
-# Start solvent calculations
-n_added = xtb_solv.add_geometries(xtb_geom)
-print(f'Added {n_added} new molecules to XTB solvent computer')
+# Start solvent calculations for adiabatic
+#  Loop over combinations of geometry datasets to source geometries and
+#   solvation energy datasets to start them
+for geom, solv in [(xtb_geom, xtb_solv), (smb_geom, smb_solv), (nbn_geom, nbn_solv)]:
+    n_added = solv.add_geometries(geom)
+    print(f'Added {n_added} new molecules from {geom.coll.name} to {solv.coll.name}')
 
-n_started = xtb_solv.start_computation(_xtb_solvs)
-print(f'Started {n_started} new solvent XTB computations')
+    n_started = solv.start_computation()
+    print(f'Started {n_started} new computations for {solv.coll.name}')
+        
+def submit_vertical_geometries(geom_dataset: GeometryDataset, vert_datasets: List[SolvationEnergyDataset]):
+    """Submit solvation energy computations for vertical geometries"""
+    all_geoms = geom_dataset.get_geometries()
+    print(f'Found {len(all_geoms)} molecules in {geom_dataset.coll.name}')
+    for inchi, geoms in all_geoms.items():
+        # Get the neutral geometry
+        if 'neutral' not in geoms:
+            continue
+        geom = geoms['neutral'].to_string('xyz')
 
-n_added = sbn_solv.add_geometries(sbn_geom)
-print(f'Added {n_added} new molecules to NWChem solvent computer')
+        # Start the neutral geometry in all three charge states
+        for postfix, charge in zip(['reduced', 'neutral', 'oxidized'], [-1, 0, 1]):
+            # Make a name
+            if charge != 0:
+                identifier = f'{inchi}_{geom_dataset.qc_spec}_neutral_{postfix}'
+            else:
+                identifier = f'{inchi}_{geom_dataset.qc_spec}_neutral'
+            new_geom = Molecule.from_data(geom, 'xyz', molecular_charge=charge, name=identifier)
+            # Loop over the different levels of accuracy
+            for vert in vert_datasets:
+                vert.add_molecule(new_geom, inchi, save=False)
+                
+    for vert in vert_datasets:  # Start the computations
+        vert.coll.save()
+        vert_started = vert.start_computation()
+        print(f'Started {vert_started} computations for {vert.coll.name}')
 
-n_started = sbn_solv.start_computation(_nwc_solvs)
-print(f'Started {n_started} new solvent NWChem computations')
+submit_vertical_geometries(xtb_geom, [smb_xtb_vert_solv, nbn_xtb_vert_solv, dif_xtb_vert_solv])
